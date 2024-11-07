cbecfa3639e6357dd7478caa3b96d27a3e8d5252
Reestart-sai <varrisaiwork@gmail.com>
2024-11-07 12:35:35 +0530

    Add files via upload

--- src/components/SiteMap.js
+++ src/components/SiteMap.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const SiteMap = () => {
-    return (
-        <div className="sitemap">
-            <h2>Sitemap</h2>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/all-private-jobs">All Private Jobs</Link></li>
-                <li><Link to="/all-government-jobs">All Government Jobs</Link></li>
-                <li><Link to="/all-internships">All Internships</Link></li>
-                <li><Link to="/all-abroad-jobs">All Abroad Jobs</Link></li>
-                <li><Link to="/contact-us">Contact Us</Link></li>
-                <li><Link to="/about-us">About Us</Link></li>
-            </ul>
-        </div>
-    );
+  useEffect(() => {
+    window.location.href = '/sitemap.xml';
+  }, []);
+  
+  return null;
 };
 
 export default SiteMap;
