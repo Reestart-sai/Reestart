f482f02d9d78db0b3a8c2ad06a4089b26f7b247c
Reestart-sai <varrisaiwork@gmail.com>
2024-11-08 00:00:43 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
@@ -11,6 +12,9 @@ import AllPrivateJobs from './components/AllPrivateJobs';
 import AllGovernmentJobs from './components/AllGovernmentJobs';
 import AllInternships from './components/AllInternships';
 import AllAbroadJobs from './components/AllAbroadJobs';
+
+import SiteMap from './components/SiteMap';
+
 import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -32,14 +36,14 @@ const App = () => {
       cachedJobs && cacheTimestamp && Date.now() - cacheTimestamp < CACHE_EXPIRATION_TIME;
 
     if (isCacheValid) {
-      setJobs(JSON.parse(cachedJobs)); // Using setJobs here
+      setJobs(JSON.parse(cachedJobs));
     } else {
       // Cache jobs data in local storage and update timestamp
       localStorage.setItem('cachedJobs', JSON.stringify(jobsData));
       localStorage.setItem('cacheTimestamp', Date.now());
-      setJobs(jobsData); // Using setJobs here
+      setJobs(jobsData);
     }
-  }, []); // This effect runs once on component mount
+  }, []);
 
   // Handle search functionality with caching
   const handleSearch = (query) => {
@@ -49,7 +53,7 @@ const App = () => {
       cachedSearch && cacheTimestamp && Date.now() - cacheTimestamp < CACHE_EXPIRATION_TIME;
 
     if (isCacheValid) {
-      setSearchResults(JSON.parse(cachedSearch)); // Using setSearchResults here
+      setSearchResults(JSON.parse(cachedSearch));
     } else {
       const results = jobs.filter(
         (job) =>
@@ -57,14 +61,14 @@ const App = () => {
           job.companyName.toLowerCase().includes(query.toLowerCase()) ||
           job.experience.toLowerCase().includes(query.toLowerCase())
       );
-      setSearchResults(results); // Using setSearchResults here
+      setSearchResults(results);
 
       // Cache the search results and update timestamp
       localStorage.setItem(`search_${query}`, JSON.stringify(results));
       localStorage.setItem(`searchTimestamp_${query}`, Date.now());
     }
 
-    navigate('/search-results'); // Navigating to search results
+    navigate('/search-results');
   };
 
   return (
@@ -80,6 +84,7 @@ const App = () => {
         <Route path="/all-abroad-jobs" element={<AllAbroadJobs jobs={jobs} />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/sitemap.xml" element={<SiteMap />} /> 
       </Routes>
       <Footer />
     </>
