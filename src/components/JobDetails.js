26476cfd70a6285a4a70542f2be61ec494ce6695
Reestart-sai <varrisaiwork@gmail.com>
2024-11-24 11:26:34 +0530

    JobDetails.js

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -41,19 +41,30 @@ const JobDetails = () => {
   return (
     <>
       <Helmet>
-        <title>{job.companyName} - {job.jobRole} | Reestarts</title>
+        <title>{job.companyName} - {job.jobRole} | Restarts</title>
         <meta name="description" content={`Apply for ${job.jobRole} at ${job.companyName}.`} />
-        <meta name="keywords" content={`${job.companyName}, ${job.jobRole}, jobs, careers, Reestarts,reestarts,restarts,restart`} />
+        <meta name="keywords" content={`${job.companyName}, ${job.jobRole}, jobs, careers, Restarts`} />
       </Helmet>
       <div className="job-details">
         <h2>{job.companyName}</h2>
         {job.companyImage && <img src={job.companyImage} alt={job.companyName} />}
-        <p>Job Role: {job.jobRole}</p>
-        <p>Posted on: {new Date(job.postDate).toLocaleDateString()}</p>
-        <p>Experience: {job.experience}</p>
-        <p>Category: {job.category}</p>
-        <p>Qualification: {job.qualification}</p>
-        <p>Skills: {Array.isArray(job.skills) ? job.skills.join(', ') : job.skills || 'No skills listed'}</p>
+        <p><strong>Job Role:</strong> {job.jobRole}</p>
+        <p><strong>Posted on:</strong> {new Date(job.postDate).toLocaleDateString()}</p>
+        <p><strong>Experience:</strong> {job.experience}</p>
+        {job.vacancy && (
+          <p>
+              <strong>Job Vacancy:</strong> {job.vacancy}
+          </p>
+        )}
+
+        <p><strong>Category:</strong> {job.category}</p>
+        <p><strong>Qualification:</strong> {job.qualification}</p>
+        {job.vacancy && (
+          <p>
+              <strong>Skills:</strong> {Array.isArray(job.skills) ? job.skills.join(', ') : job.skills || 'No skills listed'}
+          </p>
+        )}
+        
         <div>
           <h3>Job Description:</h3>
           {jobDescription.length > 0 ? (
@@ -69,9 +80,16 @@ const JobDetails = () => {
         <p>
           <a href={job.jobLink} target="_blank" rel="noopener noreferrer">Apply Here</a>
         </p>
+        {job.notification &&(
+          <p className="notification">
+            <a href={job.notification} target="_blank" rel="noopener noreferrer">
+               Notifications PDF
+            </a>
+          </p>
+        )}
         {job.telegramLink && (
           <p>
-            <a href={job.telegramLink} target="_blank" rel="noopener noreferrer">Join Whatsapp for Latest Updates</a>
+            <a href={job.telegramLink} target="_blank" rel="noopener noreferrer">Join Telegram for Updates</a>
           </p>
         )}
         <Link to="/" className="back-to-home">Back to Home</Link>
