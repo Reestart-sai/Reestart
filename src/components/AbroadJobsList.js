d334d5260576896652452573a4a636323c386734
Reestart-sai <varrisaiwork@gmail.com>
2024-11-26 20:51:17 +0530

    AbroadJobsList.js

--- src/components/AbroadJobsList.js
+++ src/components/AbroadJobsList.js
@@ -4,7 +4,7 @@ import JobCardList from './JobCardList';
 import { Link } from 'react-router-dom';
 import '../styles/AbroadJobsList.css';
 
-const CACHE_EXPIRATION =10 * 60 * 1000; // 24 hours in milliseconds
+const CACHE_EXPIRATION = 2 * 60 * 1000; // 24 hours in milliseconds
 
 const sortJobsByDate = (jobs) => jobs.sort((a, b) => new Date(b.postDate) - new Date(a.postDate));
 
